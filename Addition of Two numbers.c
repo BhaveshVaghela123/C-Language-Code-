888cdab1778d36ee9ca0a20346836719a4f6514e
Bhavesh Vaghela <bhaveshvaghela1018@gmail.com>
2026-08-12 21:56:38 +0530

    Add files via upload

---
+++ Addition of Two numbers.c
@@ -0,0 +1,13 @@
+// Addition of Two numbers //
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a, b, sum;
+    printf("Enter the number:");
+    scanf("%d",&a);
+    printf("Enter the number:");
+    scanf("%d",&b);
+    sum = a+b ;
+    printf("Sum = %d",sum);
+}
