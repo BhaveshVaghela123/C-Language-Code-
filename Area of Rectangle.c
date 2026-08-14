2d19b47992c827b82c2f965fb852d548f4e10fd6
Bhavesh Vaghela <bhaveshvaghela1018@gmail.com>
2026-08-14 22:50:11 +0530

    Add files via upload

---
+++ Area of Rectangle.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    float length, width, area;
+
+    printf("Enter length: ");
+    scanf("%f", &length);
+
+    printf("Enter width: ");
+    scanf("%f", &width);
+
+    area = length * width;
+
+    printf("Area of Rectangle = %f", area);
+
+
+}
