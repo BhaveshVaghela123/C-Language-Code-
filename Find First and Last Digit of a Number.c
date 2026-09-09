0a633aba5a8a13de2beb9bede7661fee35237bfd
Bhavesh Vaghela <bhaveshvaghela1018@gmail.com>
2026-09-09 20:24:19 +0530

    Add files via upload

---
+++ Find First and Last Digit of a Number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, first, last;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    last = num % 10;
+
+    while (num >= 10)
+    {
+        num = num / 10;
+    }
+
+    first = num;
+
+    printf("First digit = %d\n", first);
+    printf("Last digit = %d", last);
+
+    return 0;
+}
